a9dc90a4cdc297a999e2f7e7f839c02d0192e75c
louisvincelli <79131518+louisvincelli@users.noreply.github.com>
2021-09-03 22:30:21 -0700

    final

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Order = require("./Orders");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  orders: [Order.Schema],
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userschema);
+
+module.exports = User;
---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Category = require('./Category');
+const Product = require('./Product');
+const Orders = require('./Orders');
+
+module.exports = { User, Category, Product, Orders };
